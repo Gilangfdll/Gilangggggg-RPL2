87fbe2af5a0b9489da2cd870bd762c0888d95b6d
Gilangfdll <98387841+Gilangfdll@users.noreply.github.com>
2022-01-29 15:09:44 +0700

    Add files via upload

---
+++ array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Array</title>
+</head>
+<body>
+    <h2><i>Masukkan Nama</i></h2>
+    <form action="array1.php" method="post">
+        <ol>
+        <?php
+        for ($a = 1; $a <= 9; $a++) {
+            echo "<li> <input type='text' name='nama[$a]'>";
+        }
+        ?>
+        </ol><input type="submit" value="Proses" />
+    </form>
+</body>
+</html>
